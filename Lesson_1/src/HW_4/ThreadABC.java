e8d28610417b670fa03ab0d55dc9409d0bf84014
Yuriy <yuriy.filimonov@gmail.com>
2020-05-25 13:56:16 +0300

    Task 1 completed.

--- Lesson_1/src/HW_4/ThreadABC.java
+++ Lesson_1/src/HW_4/ThreadABC.java
@@ -1,4 +1,61 @@
 package HW_4;
 
-public class ThreadABC {
+public class ThreadABC extends Thread {
+    private volatile char currentLetter = 'A';
+
+
+//    public ThreadABC() {
+//        start();
+//    }
+
+//    public void run() {
+//        symbolA();
+//        symbolB();
+//        symbolC();
+//    }
+
+    public synchronized void symbolA() {
+        try {
+            for (int i = 0; i < 3; i++) {
+                while (currentLetter != 'A') {
+                    wait();
+                }
+                System.out.println("A");
+                currentLetter = 'B';
+                notifyAll();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void symbolB() {
+        try {
+            for (int i = 0; i < 3; i++) {
+                while (currentLetter != 'B') {
+                    wait();
+                }
+                System.out.println("B");
+                currentLetter = 'C';
+                notifyAll();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void symbolC() {
+        try {
+            for (int i = 0; i < 3; i++) {
+                while (currentLetter != 'C') {
+                    wait();
+                }
+                System.out.println("C");
+                currentLetter = 'A';
+                notifyAll();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
